5819758b5420baab349b36d0bc7e388a28bb2239
asaezper <alfredo.saez@srs.io>
2023-06-07 18:55:49 +0200

    ci: verbose release and merge dryrun on nightly

--- .gitlab/ci/auto_merge.sh
+++ .gitlab/ci/auto_merge.sh
@@ -20,27 +20,42 @@ set -e
 
 main() {
     # Check number of args
-    if (($# != 2)); then
+    if [ $# != 2 ] && [ $# != 3 ]; then
         echo >&2 "Please call script with source ref and target ref as argument"
-        echo >&2 "E.g. ./auto_merge.sh <source_ref> <target_ref>"
+        echo >&2 "E.g. ./auto_merge.sh <source_ref> <target_ref> [dryrun]"
         exit 1
     fi
 
     local source_ref=$1
     local target_ref=$2
+    local mode="${3:-push}"
 
     # Create filtered ref of source ref
+    echo "==================="
+    echo "= Checkout source ="
+    echo "==================="
     git fetch -q origin "$source_ref"
     git checkout -b "filtered_$source_ref" FETCH_HEAD
+    echo "===================="
+    echo "= Filtering source ="
+    echo "===================="
     git filter-repo --refs "filtered_$source_ref" --path .gitlab-ci.yml --path .gitlab/ --path .gitattributes --path-glob "*.tar.gz" --invert-paths --force
 
-    # Checkout target ref
+    # Checkout target ref and merge
+    echo "==================="
+    echo "= Checkout target ="
+    echo "==================="
     git fetch -q origin "$target_ref"
     git checkout -b "$target_ref" FETCH_HEAD
-    git rebase "filtered_$source_ref"
+    echo "========="
+    echo "= Merge ="
+    echo "========="
+    git merge --ff "filtered_$source_ref" -m "Update main"
 
     # Push
-    git push origin "$target_ref"
+    if [ "$mode" = "push" ]; then
+        git push origin "$target_ref"
+    fi
 }
 
 main "$@"
