4d6aaf8af5759e74f257df0c93a43e7accf8b696
dvdgrgrtt <davidg@srs.io>
2026-04-24 13:30:45 +0200

    lib: add support for 120kHz in scs2cps
    Signed-off-by: David Gregoratti <davidg@srs.io>

--- +ocuduLib/+ran/+utils/scs2cps.m
+++ +ocuduLib/+ran/+utils/scs2cps.m
@@ -6,10 +6,23 @@
 % to additional licensing requirements.
 
 function cpDurations = scs2cps(scs)
+    % See TS38.211 Section 5.3.1 for details.
     if (scs == 15)
+        % When SCS = 15 kHz, the CP is 160 kappa for symbols 0 and 7, and 144 kappa
+        % for the remaining symbols.
         cpDurations = [160 144 144 144 144 144 144 160 144 144 144 144 144 144];
     elseif (scs == 30)
-        cpDurations = [160 144 144 144 144 144 144 144 144 144 144 144 144 144];
+        % When SCS = 30 kHz, the CP is 88 kappa for symbols 0 and 14, and 72 kappa
+        % for the remaining symbols. Here, we only focus on the first slot in the
+        % subframe.
+        cpDurations = [88 72 72 72 72 72 72 72 72 72 72 72 72 72];
+    elseif ((scs == 30) || (scs == 120))
+        % When SCS = 120 kHz, the CP is 34 kappa for symbols 0 and 56, and 18 kappa
+        % for the remaining symbols. Here, we only focus on the first slot in the
+        % subframe.
+        cpDurations = [34 18 18 18 18 18 18 18 18 18 18 18 18 18];
+    else
+        error('ocudu_matlab:scs2cps', 'Unsupported subcarrierspacing %d kHz.', scs);
     end
     cpDurations = cpDurations / sum(cpDurations) / scs;
 end
